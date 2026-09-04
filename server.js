fb490ddf202a04c2b65baa7e5def76c62ca88014
ankur <aankur00000@gmail.com>
2026-09-04 20:32:37 +0530

    Fix CORS configuration

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-// 'Access-Control-Allow-Origin'
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import { configDotenv } from 'dotenv';
-configDotenv();
-import mydatabase from './db_connection/connectdb.js';
 
-import routing from './routing/approuting.js'
+configDotenv();
+
+import mydatabase from './db_connection/connectdb.js';
+import routing from './routing/approuting.js';
+
 const myapp = express();
 const port = process.env.PORT;
 
 myapp.use(express.json());
-myapp.use(express.urlencoded({ extended: true }))
-myapp.use(cors())
+myapp.use(express.urlencoded({ extended: true }));
+
+myapp.use(
+    cors({
+        origin: [
+            "http://localhost:3000"
+        ],
+        credentials: true
+    })
+);
+
 myapp.use(routing);
 
 myapp.listen(port, "0.0.0.0", () => {
     console.log("node is running on: ", port);
-})
+});
